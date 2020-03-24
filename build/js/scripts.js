7024866c9fb8e53d5d7b438df373d1aafb04e82b
Misha Evtukh <mishamiron2317@gmail.com>
2020-03-24 15:40:52 +0200

    managing webpack assembly

---
+++ build/js/scripts.js
@@ -0,0 +1,41 @@
+"use strict";
+
+var selectNode = function selectNode(element) {
+  return document.querySelector(element);
+};
+
+var refs = {
+  menuToggler: selectNode('.menu-toggle'),
+  body: selectNode('body'),
+  closeIcon: selectNode('.fa-times')
+};
+refs.menuToggler.addEventListener('click', function () {
+  refs.body.classList.toggle('open');
+  refs.closeIcon.classList.toggle('close');
+}); // scroll reveal
+
+window.sr = ScrollReveal();
+sr.reveal('.animate-left', {
+  origin: 'left',
+  duration: 1000,
+  distance: '25rem',
+  delay: 300
+});
+sr.reveal('.animate-right', {
+  origin: 'right',
+  duration: 1000,
+  distance: '25rem',
+  delay: 600
+});
+sr.reveal('.animate-top', {
+  origin: 'top',
+  duration: 1000,
+  distance: '25rem',
+  delay: 600
+});
+sr.reveal('.animate-bottom', {
+  origin: 'bottom',
+  duration: 1000,
+  distance: '25rem',
+  delay: 600
+});
